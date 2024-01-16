0783ed6ad0a9422f164c2f6ab1cd636888e1f1ec
Stephen Walsh <sfwalshappdev@gmail.com>
2024-01-16 12:39:30 +0000

    Create Xcode project

---
+++ GitHubExplorer/GitHubExplorer/GitHubExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExplorerApp.swift
+//  GitHubExplorer
+//
+//  Created by Stephen Walsh on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

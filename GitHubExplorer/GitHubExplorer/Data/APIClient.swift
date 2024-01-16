8632cdf5dba154ecce240cebcf80977009064443
Stephen Walsh <sfwalshappdev@gmail.com>
2024-01-16 19:41:53 +0000

    Authenticate requests with personal access token.
    for convenience, throws a reminder error to set the token in the scheme

--- GitHubExplorer/GitHubExplorer/Data/APIClient.swift
+++ GitHubExplorer/GitHubExplorer/Data/APIClient.swift
@@ -14,6 +14,15 @@ protocol APIClient {
 
 struct DefaultAPIClient: APIClient {
     func fetchData<T: Decodable>(with request: URLRequest) -> AnyPublisher<T, Error> {
+        
+        guard let githubToken = ProcessInfo.processInfo.environment["GITHUB_TOKEN"] else {
+            // NOTE: fatalError is used only here for the coding challenge purposes. It would never be used in a real environment.
+            fatalError("Please set your personal access token in the scheme's environment variables under the key `GITHUB_TOKEN`")
+        }
+        
+        var authenticatedRequest = request
+        authenticatedRequest.addValue("Bearer \(githubToken)", forHTTPHeaderField: "Authorization")
+        
         return URLSession.shared.dataTaskPublisher(for: request)
             .map(\.data)
             .decode(type: T.self, decoder: JSONDecoder())
